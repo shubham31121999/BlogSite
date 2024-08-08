a19e1227720af9734d6b36ddef549115c9fee452
shubham31121999 <65241730+shubham31121999@users.noreply.github.com>
2024-08-08 19:22:42 +0530

    Removing Vercel

--- Blog/build_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-
-echo "Build Start"
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
-echo " Build END"
--- Blog/config/django/base.py
+++ Blog/config/django/base.py
@@ -126,10 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'socialsite/static'),  # Add paths to your static directories here
-]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
